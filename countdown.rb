d4dd6cdff2789392c6cbe204a520cdff0ea8d85c
manon jacquin <manon.jacquin@gmail.com>
2018-10-03 18:23:20 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,15 +2,15 @@
 
 def countdown(number)
   
-  while number >= 0
+  while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
-    return "HAPPY NEW YEAR!"
   end
+  return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
-  while number >= 0 
+  while number > 0 
     puts "#{number} SECOND(S)!"
     number -= 1 
     sleep 1 
